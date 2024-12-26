c0a77769433f10fb519a1e2aa621e2c36938734a
Luis Garrido J. <garrlis@gmail.com>
2024-12-26 20:53:54 -0300

    Se sube con variables de bd de railway

--- src/database/connection.py
+++ src/database/connection.py
@@ -6,10 +6,17 @@ from mysql.connector import MySQLConnection
 
 def get_connection() -> MySQLConnection:
     return mysql.connector.connect(
-        host=os.getenv('MYSQL_HOST'),
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASS'),
-        database=os.getenv('MYSQL_DB'),
-        port=os.getenv('MYSQL_PORT',3306),
+        host=os.getenv('MySQL.MYSQLHOST'),
+        user=os.getenv('MySQL.MYSQLUSER'),
+        password=os.getenv('MySQL.MYSQLPASSWORD'),
+        database=os.getenv('MySQL.MYSQLDATABASE'),
+        port=os.getenv('MySQL.MYSQLPORT',3306),
     )
+
+
+""" host=os.getenv('MYSQL_HOST'),
+user=os.getenv('MYSQL_USER'),
+password=os.getenv('MYSQL_PASS'),
+database=os.getenv('MYSQL_DB'),
+port=os.getenv('MYSQL_PORT',3306), """
 
