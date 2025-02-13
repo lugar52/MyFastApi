ce9978a99143734474da6605769832fa22744d5e
Luis Garrido J. <garrlis@gmail.com>
2025-02-13 00:31:03 -0300

    modif query perneria

--- src/services/queryPerneria.py
+++ src/services/queryPerneria.py
@@ -8,7 +8,7 @@ class Querys_perneria:
     P.OBSERVACION, P.NB_ASIG_TERR, TP.DESCRIPCION as TIPOELEM_DESCRIPCION,  TU.DESCRIPCION as TUNEL_DESCRIPCION,  DI.DESCRIPCION as DISPO_DESCRIPCION, \
     PR.DESCRIPCION as PROVE_DESCRIPCION, PA.DESCRIPCION as PATIO_DESCRIPCION, \
     ROUND((CANTIDAD_TERRENO/CANTIDAD_SNF)*100, 2) AS PORCENTAJE, 0 as isEdit, false as isSelected, P.CANT_DESPACHOS , P.STOCK , \
-    SP.Descripcion as SUBPATIO_DESCRIPCION, ifnull(SP.ID_SubPatio, 0) AS ID_SubPatio , C.Descripcion AS COORDENADA_DESCRIPCION, ifnull(C.ID_Coordenada, 0) AS ID_Coordenada  \
+    SP.Descripcion as SUBPATIO_DESCRIPCION, ifnull(SP.ID_SubPatio, 0) AS ID_SubPatio , C.Descripcion AS COORDENADA_DESCRIPCION, ifnull(C.ID_Coordenada, 0) AS ID_Coordenada, P.GUIA_PROVEEDOR, P.UNIDAD, P.CAJON_PALLET_JAVA, P.RECEPCIONADO, P.ELEMENTO, e.descripcion AS ELEMENTO_DESC  \
     FROM control_perneria P \
         LEFT JOIN TIPO_ELEMENTO TP ON P.TIPO_ELEMENTO = TP.ID_TPELEMENTO \
         LEFT JOIN TUNEL TU ON P.TUNEL = TU.ID_TUNEL \
@@ -16,4 +16,5 @@ class Querys_perneria:
         LEFT JOIN proveedor PR ON P.PROVEEDOR = PR.ID_PROVEEDOR  \
         LEFT JOIN PATIO PA ON P.PATIO = PA.ID_PATIO \
         LEFT JOIN SubPatio SP ON P.SUB_PATIO = SP.ID_SubPatio \
-        LEFT JOIN Coordenadas C on P.COORDENADAS = C.ID_Coordenada"
+        LEFT JOIN Coordenadas C on P.COORDENADAS = C.ID_Coordenada \
+        LEFT JOIN elementos e on P.ELEMENTO = e.Id_elementos " 
